3be200ce6da697c04f33613a0bd5f84ed051cffb
jacobponce <ponce.jacob.t@gmail.com>
2024-10-10 23:19:15 -0700

    Getting users by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -55,6 +55,21 @@ app.get("/users", (req, res) => {
     res.send(users);
   }
 });
+ 
+// This helper function makes it easier to refactor and edit later on.
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id); // find gets the first occurrence of unique identifier
+  
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send(result);
+    }
+});
+
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
