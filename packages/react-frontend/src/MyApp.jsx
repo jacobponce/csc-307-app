22fec9cc2c0b9a0bd152244d4411e10a85020251
jacobponce <ponce.jacob.t@gmail.com>
2024-10-14 21:03:59 -0700

    IE3 Submission

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,27 +1,84 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
-  function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
-      return i !== index;
+  function deleteUser(id) {
+    const promise = fetch(`http://localhost:8000/users/${id}`, {
+      method: "DELETE",
     });
-    setCharacters(updated);
+    return promise;
   }
+
+  function removeOneCharacter(id, index) {
+    deleteUser(id)
+      .then((res) => {
+        if (res.status === 204) {
+          const updated = characters.filter((character, i) => {
+            return i !== index;
+          });
+          setCharacters(updated);
+          console.log(`User with id ${id} deleted.`);
+        } else {
+          console.log(`Unexpected status code: ${res.status}`);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   function updateList(person) {
-    setCharacters([...characters, person]);
+    postUser(person)
+      .then((res) => {
+        if (res.status === 201) {
+          return res.json();
+        } else {
+          console.log(`Unexpected status code: ${res.status}`);
+        }
+      })
+      .then((data) => {
+        if (data) {
+          setCharacters([...characters, data]);
+          console.log(data);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
-  
+
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  function postUser(person) {
+    const promise = fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div className="container">
-      <Table
-        characterData={characters}
-        removeCharacter={removeOneCharacter}
-      />
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
       <Form handleSubmit={updateList} />
     </div>
   );
